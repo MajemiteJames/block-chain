ca43a3643a3683f9a9a426bcfb0c7d9b5afcf42b
MajemiteJames <majemiteokoro@gmail.com>
2021-09-15 13:51:20 +0100

    transaction mining functionality and test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
 const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
+const TransactionMiner = require('./app/transaction-miner');
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain, transactionPool, wallet  });
+const transactionMiner = new TransactionMiner({ blockchain, transactionPool, wallet, pubsub });
 
 const DEFAULT_PORT = 3100;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -61,20 +63,20 @@ app.post('/api/transact', (req, res) => {
     res.json(transactionPool.transactionMap);
   });
   
-//   app.get('/api/mine-transactions', (req, res) => {
-//     transactionMiner.mineTransactions();
+  app.get('/api/mine-transactions', (req, res) => {
+    transactionMiner.mineTransactions();
   
-//     res.redirect('/api/blocks');
-//   });
+    res.redirect('/api/blocks');
+  });
   
-//   app.get('/api/wallet-info', (req, res) => {
-//     const address = wallet.publicKey;
+  // app.get('/api/wallet-info', (req, res) => {
+  //   const address = wallet.publicKey;
   
-//     res.json({
-//       address,
-//       balance: Wallet.calculateBalance({ chain: blockchain.chain, address })
-//     });
-//   });
+  //   res.json({
+  //     address,
+  //     balance: Wallet.calculateBalance({ chain: blockchain.chain, address })
+  //   });
+  // });
   
 
 const syncWithRootState  = () => {
