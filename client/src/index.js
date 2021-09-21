41c79df16b7d313cfbd47835b6a6d7bdb133f2c6
MajemiteJames <majemiteokoro@gmail.com>
2021-09-21 21:40:57 +0100

    setup the app file

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App'
 
 render(
-  <div>CryptoChain Frontend Live updates third fourth fifth</div>,
+  <App />,
    document.getElementById('root')
 );
