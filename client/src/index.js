50d6ea10d1ce0eccf8518e6e04f68db2e7164273
MajemiteJames <majemiteokoro@gmail.com>
2021-09-23 12:23:54 +0100

    created  atranacr=tion page

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { Router, Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Blocks from '../src/components/Blocks';
+import ConductTransaction from '../src/components/ConductTransaction';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.render(
       <Switch>
         <Route exact path='/' component={App} />
         <Route path='/blocks' component={Blocks} />
+        <Route path='/conduct-transaction' component={ConductTransaction} />
       </Switch>
     </Router>
   </React.StrictMode>,
